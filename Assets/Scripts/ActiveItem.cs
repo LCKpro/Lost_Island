0336f2d3f5a661cccc6b8be4bf181a328818dcdb
LCKpro <tjdwls9981@naver.com>
2022-03-05 15:03:12 +0900

    인벤토리추가(미완)+액티브아이템 스크립트
    인벤토리매니저 스크립트
    액티브아이템 직렬화

---
+++ Assets/Scripts/ActiveItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class ActiveItem
+{
+    public int damage;
+    public Sprite sprite;
+    public int activeNum;
+    public int amount;
+}
---
+++ Assets/Scripts/ActiveItemBook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveItemBook : MonoBehaviour
+{
+    public ActiveItem[] activeItems;
+
+    public ActiveItem CastActiveItem(int index)
+    {
+        return activeItems[index];
+    }
+}
